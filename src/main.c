50ef75a759bb6d8f5285c3a17a4bc3415c5b4acd
Nikita Buyevich <nikita.buyevich@gmail.com>
2020-04-26 17:47:17 -0500

    refactor: flags array implementation

--- src/main.c
+++ src/main.c
@@ -7,19 +7,21 @@
 
 int main(int argc, char *argv[])
 {
+    // Update index if order is modified
     Flag flags[] = {
         {.name = "class", .usage = "Process class name to kill. e.g., \"ConsoleWindowClass\""},
         {.name = "src", .usage = "Source to copy contents from. Must be a full path."},
         {.name = "dst", .usage = "Destination to copy contents to. Must be a full path."}
     };
     int flagsLen = sizeof(flags) / sizeof(flags[0]);
+    cmd_parse_str(&flags, flagsLen, argc, argv);
+    char *classVal = flags[0].val;
+    char *srcVal = flags[1].val;
+    char *dstVal = flags[2].val;
 
-//    cmd_parse_str(&flags, flagsLen, argc, argv);
-//    printf("lol %s\n", flags[0].val);
-////
-//    wnd_kill_processes_by_class_name(className);
-//    cp(dst, src);
-//
-//    cmd_free_flags(&flags, flagsLen);
+    wnd_kill_processes_by_class_name(classVal);
+    cp(dstVal, srcVal);
+
+    cmd_free_flags(&flags, flagsLen);
     return 0;
 }
